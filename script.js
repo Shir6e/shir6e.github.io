1ee90d3b37f72bacdb16bc5ceaa7663f1a68592c
Shir6e <pratamaazka007@gmail.com>
2025-12-01 10:29:04 +0700

    Mantap

--- script.js
+++ script.js
@@ -28,3 +28,42 @@ document.querySelectorAll("#navbar a").forEach(link => {
     });
 });
 
+const words = [
+    "Pelajar",
+    "Gamer",
+    "Perlaukan",
+    "Tolak Angin",
+    "Orang Biasa :)"
+];
+
+let index = 0;
+let charIndex = 0;
+let isDeleting = false;
+const typingElement = document.getElementById("typing");
+
+function typeEffect() {
+    let currentWord = words[index];
+    
+    if (!isDeleting) {
+        typingElement.textContent = currentWord.substring(0, charIndex + 1);
+        charIndex++;
+
+        if (charIndex === currentWord.length) {
+            isDeleting = true;
+            setTimeout(typeEffect, 1000); // jeda sebelum menghapus
+            return;
+        }
+    } else {
+        typingElement.textContent = currentWord.substring(0, charIndex - 1);
+        charIndex--;
+
+        if (charIndex === 0) {
+            isDeleting = false;
+            index = (index + 1) % words.length;
+        }
+    }
+
+    setTimeout(typeEffect, isDeleting ? 60 : 100); 
+}
+
+typeEffect();
