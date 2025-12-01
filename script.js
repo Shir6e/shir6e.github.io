3ac7eeb2438f2f4f3d6ef482b5b0e56509c5afad
Shir6e <pratamaazka007@gmail.com>
2025-12-01 10:07:52 +0700

    mempolish

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
+// fade in animation
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if (entry.isIntersecting){
             entry.target.classList.add("show");
         }
     });
 });
 
-const hiddenElements = document.querySelectorAll(".fade-in");
-hiddenElements.forEach(el => observer.observe(el));
+document.querySelectorAll('.fade-in').forEach(el=>{
+    observer.observe(el);
+});
 
 const menuIcon = document.getElementById("menu-icon");
 const navbar = document.getElementById("navbar");
 
+// toggle menu
 menuIcon.addEventListener("click", () => {
     navbar.classList.toggle("active");
-    menuIcon.classList.toggle("open"); // animasi burger -> X
+    menuIcon.classList.toggle("open");
 });
+
+// auto close when clicking a link
+document.querySelectorAll("#navbar a").forEach(link => {
+    link.addEventListener("click", () => {
+        navbar.classList.remove("active");
+        menuIcon.classList.remove("open");
+    });
+});
+
